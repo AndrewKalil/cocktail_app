ef87abbdb363271997e8393c475ef2fc5e0bd978
AndrewKalil <1541@holbertonschool.com>
2021-04-07 23:31:50 -0500

    Updates

---
+++ precache-manifest.8cb98d262827028bd3521dba1ca3579e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8893a5c9186f8333e4f8ebccaf8fdb86",
+    "url": "/cocktail_app/index.html"
+  },
+  {
+    "revision": "f5f5a3c60529bbd3b9f8",
+    "url": "/cocktail_app/static/css/main.c1d01b53.chunk.css"
+  },
+  {
+    "revision": "d74c469cf8467746e26b",
+    "url": "/cocktail_app/static/js/2.ce8c6936.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/cocktail_app/static/js/2.ce8c6936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5f5a3c60529bbd3b9f8",
+    "url": "/cocktail_app/static/js/main.60bd50d2.chunk.js"
+  },
+  {
+    "revision": "8f0f02eb74b2bd7a70c2",
+    "url": "/cocktail_app/static/js/runtime-main.f8c84f9a.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/cocktail_app/static/media/logo.9a3d2645.svg"
+  }
+]);
