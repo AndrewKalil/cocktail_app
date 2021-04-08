c49f998362fd2a4d63058a9be63e14b0159fb88d
AndrewKalil <1541@holbertonschool.com>
2021-04-07 23:43:33 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cocktail_app/precache-manifest.332c597db75ca04ed729bdb6409d4134.js"
+  "/cocktail_app/precache-manifest.7e55a8281a47d581f1b25ee6b4ac09a2.js"
 );
 
 self.addEventListener('message', (event) => {
