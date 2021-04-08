358e29e950e7df553c75d674a462bfdf89b7f31e
AndrewKalil <1541@holbertonschool.com>
2021-04-07 23:36:39 -0500

    adding files

---
+++ build/precache-manifest.a0eb19c452aee06a3d4740144f750a02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104002d0672966434627559c5214cd18",
+    "url": "/cocktail_app/index.html"
+  },
+  {
+    "revision": "ac860c9d21b8eb48258d",
+    "url": "/cocktail_app/static/css/main.c1d01b53.chunk.css"
+  },
+  {
+    "revision": "d74c469cf8467746e26b",
+    "url": "/cocktail_app/static/js/2.ce8c6936.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/cocktail_app/static/js/2.ce8c6936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac860c9d21b8eb48258d",
+    "url": "/cocktail_app/static/js/main.b14b6bed.chunk.js"
+  },
+  {
+    "revision": "8f0f02eb74b2bd7a70c2",
+    "url": "/cocktail_app/static/js/runtime-main.f8c84f9a.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/cocktail_app/static/media/logo.9a3d2645.svg"
+  }
+]);
---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur
+				adipisicing elit. Quisquam possimus voluptas
+				maiores voluptatibus dicta aut maxime placeat
+				nihil! Earum iusto quae porro assumenda ut
+				fuga, sunt deleniti, nihil explicabo eligendi
+				atque eius. Quo obcaecati labore esse deleniti
+				nihil aliquid saepe?
+			</p>
+		</section>
+	)
+}
+
+export default About
